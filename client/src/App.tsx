5ac64cc22afbf53266894352d84e883243b434ef
MarcosMoreno0912 <moreno83marcos@gmail.com>
2024-01-10 17:06:35 -0300

    entry point frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.module.css'
+import ContactoDeAyuda from './component/contactoDeAyuda/contactoDeAyuda.tsx'
 
 function App() {
-
   return (
-    <>
-      <p className="read-the-docs">
-        Forms typescript practice
-      </p>
-    </>
+    <Router>
+      <>
+        <Routes>
+          <Route path="/" element={<ContactoDeAyuda/>} />
+          {/*<Route path="/respuestaConsulta" element:{<respuestaConsulta/>} />*/}
+        </Routes>
+        <ToastContainer/>
+      </>
+    </Router>
   )
 }
 
